76e81d8b40ed9566cfe422f94a9064d886d93084
picodegallo <pocodegallo@gmail.com>
2013-06-26 11:59:28 -0400

    can edit students

--- app.rb
+++ app.rb
@@ -6,16 +6,6 @@ require './student.rb'
 module StudentSite
   
   class App < Sinatra::Base
-   
-    # get '/hello-world' do
-    #   @random_numbers = (1..20).to_a
-    #   erb :hello
-    # end
-     
-    # get '/artists' do
-    #   @artists = ["Led Zeppelin", "New Order", "Another Band"]
-    #   erb :artists
-    # end
      
     get '/students' do
       @id = params[:id]
@@ -27,9 +17,56 @@ module StudentSite
       redirect to "/students"
     end
 
-    get '/students/:id' do |id|
-      @student = Student.find(id)
+    get '/students/new' do
+      erb :form
+    end
+
+    post '/students/new-user' do
+      @student = Student.new
+      @student.name = params[:name]
+      @student.image_url = params[:image_url]
+      @student.quote = params[:quote]
+      @student.twitter_url = params[:twitter_url]
+      @student.linkedin_url = params[:linkedin_url]
+      @student.github_url = params[:github_url]
+      @student.blog_url = params[:blog_url]
+      @student.treehouse_url = params[:treehouse_url]
+      @student.codeschool_url = params[:codeschool_url]
+      @student.coderwall_url = params[:coderwall_url]
+      @student.save
+
       erb :'students/profile'
+    end
+
+    post '/students/:id/edited-user' do 
+      @id = params[:id]
+      @student = Student.find(params[:id])
+      @student.name = params[:name]
+      @student.image_url = params[:image_url]
+      @student.quote = params[:quote]
+      @student.twitter_url = params[:twitter_url]
+      @student.linkedin_url = params[:linkedin_url]
+      @student.github_url = params[:github_url]
+      @student.blog_url = params[:blog_url]
+      @student.treehouse_url = params[:treehouse_url]
+      @student.codeschool_url = params[:codeschool_url]
+      @student.coderwall_url = params[:coderwall_url]
+      @student.save
+
+      erb :'students/profile'
+    end
+
+    get '/students/:id' do |id|
+      @id = params[:id]
+      @student = Student.find(id)
+
+      erb :'students/profile'
+    end
+
+    get '/students/:id/edit' do |id|
+      @student = Student.find(params[:id])
+      @id = params[:id]
+      erb :edit
     end
 
   end
